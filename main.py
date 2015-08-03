9f29a50e25190ba75edcebce45b91d241592264b
umiahmed <umiahmed30@gmail.com>
2015-08-03 12:04:05 -0400

    f this

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 import random
 from google.appengine.api import users
+from google.appengine.ext import ndb
 import jinja2
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -16,12 +17,12 @@ class Student(ndb.Model):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            greeting = ('Welcome, %s!(<a href="%s">sign out</a>)'%(user.nickname(),users.create_logout_url('/')))
-        else:
-            greeting = ('<a href ="%s">Sign in or Register</a>.'% users.create_login_url('/'))
-        self.response.out.write('%s'% greeting)
+        # user = users.get_current_user()
+        # if user:
+        #     greeting = ('Welcome, %s!(<a href="%s">sign out</a>)'%(user.nickname(),users.create_logout_url('/')))
+        # else:
+        #     greeting = ('<a href ="%s">Sign in or Register</a>.'% users.create_login_url('/'))
+        # self.response.out.write('%s'% greeting)
         # Get all of the student data from the datastore
         student_query = Student.query()
         student_query = student_query.order(Student.name)
