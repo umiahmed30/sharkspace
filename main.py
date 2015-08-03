57902b77b7b10ca19cf3ffa6be0706aff4778997
umiahmed <umiahmed30@gmail.com>
2015-08-03 11:44:33 -0400

    f it

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import os
 import random
 from google.appengine.api import users
 import jinja2
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Student(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    story_input = ndb.TextProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -13,6 +22,32 @@ class MainHandler(webapp2.RequestHandler):
         else:
             greeting = ('<a href ="%s">Sign in or Register</a>.'% users.create_login_url('/'))
         self.response.out.write('%s'% greeting)
+        # Get all of the student data from the datastore
+        student_query = Student.query()
+        student_query = student_query.order(Student.name)
+        # student_query = student_query.filter(Student.name == 'Adam')
+        student_data = student_query.fetch()
+        # Pass the data to the template
+        template_values = {
+            'students' : student_data
+        }
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+    def post(self):
+        # Get the student name and university from the form
+        name = self.request.get('name')
+        story_input = self.request.get('story_input')
+        # lunchbox_instance = LunchBox(
+        # food = self.request.get('food'),
+        # drink = self.request.get('drink'),
+        # insulated = True)
+        # my_lunchbox_key = lunchbox_instance.put()
+        # Create a new Student and put it in the datastore
+        student = Student(name=name, story_input=story_input)
+        student.put()
+        # Redirect to the main handler that will render the template
+        self.redirect('/')
+
 
 
 app = webapp2.WSGIApplication([
